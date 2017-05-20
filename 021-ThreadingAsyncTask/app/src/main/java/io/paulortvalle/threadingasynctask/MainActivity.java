5f1c6e0c2cc1daa919d3bd0b174f64d2f7c8bed2
Paulo Valle <paulortvalle@gmail.com>
2017-05-20 15:10:46 -0300

    Add project 021 ThreadingAsyncTask

---
+++ 021-ThreadingAsyncTask/app/src/main/java/io/paulortvalle/threadingasynctask/MainActivity.java
@@ -0,0 +1,88 @@
+package io.paulortvalle.threadingasynctask;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private final static String TAG = "ThreadingAsyncTask";
+
+    private ImageView mImageView;
+    private ProgressBar mProgressBar;
+    private int mDelay = 500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.imageView);;
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        final Button button = (Button) findViewById(R.id.loadButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new LoadIconTask().execute(R.drawable.buzz);
+            }
+        });
+
+        final Button otherButton = (Button) findViewById(R.id.otherButton);
+        otherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "I'm Working",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    class LoadIconTask extends AsyncTask<Integer, Integer, Bitmap> {
+        // parameters, progress, bitmap
+
+        @Override
+        protected void onPreExecute() {
+            mProgressBar.setVisibility(ProgressBar.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(Integer... resId) {
+            Bitmap tmp = BitmapFactory.decodeResource(getResources(), resId[0]);
+            // simulating long-running operation
+            for (int i = 1; i < 11; i++) {
+                sleep();
+                publishProgress(i * 10);
+            }
+            return tmp;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            mProgressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+            mImageView.setImageBitmap(result);
+        }
+
+        private void sleep() {
+            try {
+                Thread.sleep(mDelay);
+            } catch (InterruptedException e) {
+                Log.e(TAG, e.toString());
+            }
+        }
+    }
+
+}
