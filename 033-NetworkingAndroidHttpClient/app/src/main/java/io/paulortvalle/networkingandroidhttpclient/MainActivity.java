925e5168470729091ac40826050b60d91335ec23
Paulo Valle <paulortvalle@gmail.com>
2017-06-05 20:12:46 -0300

    Add project 033 NetworkingAndroidHttpClient

--- 033-NetworkingAndroidHttpClient/app/src/main/java/io/paulortvalle/networkingandroidhttpclient/MainActivity.java
+++ 033-NetworkingAndroidHttpClient/app/src/main/java/io/paulortvalle/networkingandroidhttpclient/MainActivity.java
@@ -1,13 +1,73 @@
 package io.paulortvalle.networkingandroidhttpclient;
 
 import android.app.Activity;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
+
+    private TextView mTextView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView1);
+
+        final Button loadButton = (Button) findViewById(R.id.button1);
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new HttpGetTask().execute();
+            }
+        });
     }
+
+    private class HttpGetTask extends AsyncTask<Void, Void, String> {
+
+        // Get your own user name at http://www.geonames.org/login
+        private static final String USER_NAME = "aporter";
+        private static final String URL = "http://api.geonames.org/earthquakesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&username=" + USER_NAME;
+
+        AndroidHttpClient mClient = AndroidHttpClient.newInstance("");
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            HttpGet request = new HttpGet(URL);
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+            try {
+                return mClient.execute(request, responseHandler);
+            } catch (ClientProtocolException exception) {
+                exception.printStackTrace();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            if (null != mClient)
+                mClient.close();
+
+            mTextView.setText(result);
+
+        }
+    }
+
 }
